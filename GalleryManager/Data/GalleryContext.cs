1605a233a3f851645f7a96e2501d848fe16f2a18
AlexGevorkyan <gag63a@gmail.com>
2025-09-05 18:02:41 +0300

    Created database

---
+++ GalleryManager/Data/GalleryContext.cs
@@ -0,0 +1,25 @@
+﻿using GalleryManager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalleryManager.Data
+{
+    public class GalleryContext : DbContext
+    {
+        public GalleryContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=GalleryDb;Trusted_Connection=true;MultipleActiveResultSets=true;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        public DbSet<Picture> Pictures { get; set; }
+    }
+}
