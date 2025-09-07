5db5b34c4a2b44331e8b86e530c86df68c4dfb72
AlexGevorkyan <gag63a@gmail.com>
2025-09-07 13:49:01 +0300

    Added creation of folder images

--- GalleryManager/Form1.cs
+++ GalleryManager/Form1.cs
@@ -5,6 +5,21 @@ namespace GalleryManager
         public Form1()
         {
             InitializeComponent();
+
+            string localDir = "./images";
+            if(!Directory.Exists(localDir))
+            {
+                Directory.CreateDirectory(localDir);
+            }
+
         }
     }
 }
+//  ./  - current folder
+//  ..  - one level up
+//  OpenFileDialoge ofd = new OpenFileDialoge();
+//  if(ofd.ShowDialog() == DialogResult.OK)
+//  {
+//      string path = Path.Combine(localDir, Path.GetFileName(ofd.FileName));
+//      File.Copy(ofd.FileName, path, true);
+//  }
